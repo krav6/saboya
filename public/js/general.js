ea8ca813fd10a0de75e140eea89132f0b059bbb1
Szabo Patrik <kraven6@gmail.com>
2018-04-02 16:11:04 +0900

    [feat] Integrated the waypoints library

--- public/js/general.js
+++ public/js/general.js
@@ -1,16 +1,57 @@
 window.onload = function() {
-  $(".fadein")
+  $(".fade-in")
     .css("visibility", "visible")
     .hide()
     .fadeIn(3000);
 
-  $(".slideup").animate(
+  $(".slide-up").animate(
     {
       bottom: "0%"
     },
-    { duration: 2000, queue: false },
-    function() {
-      // Animation complete.
-    }
+    { duration: 2000, queue: false }
   );
 };
+
+function appearLeft() {
+    $("#" + this.element.id)
+      .animate(
+        {
+          opacity: 1,
+          left: "0%"
+        },
+        { duration: 2000, queue: false }
+      );
+
+    this.destroy();
+};
+
+function appearRight() {
+    $("#" + this.element.id)
+      .animate(
+        {
+          opacity: 1,
+          right: "0%"
+        },
+        { duration: 2000, queue: false }
+      );
+
+    this.destroy();
+};
+
+const appearRightWaypoints = $(".appear-right").map(
+  function() {
+    new Waypoint({
+      element: document.getElementById(this.id),
+      handler: appearRight,
+      offset: "75%"
+    }); }
+
+);
+
+const appearLeftWaypoints = $(".appear-left").map(function() {
+  new Waypoint({
+    element: document.getElementById(this.id),
+    handler: appearLeft,
+    offset: "75%"
+  });
+});
