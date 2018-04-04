1233281e0cab0b5d2267f5d7090b4927b5b1c203
krav6 <patrik.szabo.92@gmail.com>
2018-04-04 14:12:31 +0900

    [feat] Green redesign

--- public/js/general.js
+++ public/js/general.js
@@ -1,3 +1,14 @@
+$(document).on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
+
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
+});
+
 window.onload = function() {
   $(".effect__fade-in")
     .css("visibility", "visible")
@@ -13,45 +24,41 @@ window.onload = function() {
 };
 
 function appearLeft() {
-    $("#" + this.element.id)
-      .animate(
-        {
-          opacity: 1,
-          left: "0%"
-        },
-        { duration: 2000, queue: false }
-      );
+  $("#" + this.element.id).animate(
+    {
+      opacity: 1,
+      left: "0%"
+    },
+    { duration: 2000, queue: false }
+  );
 
-    this.destroy();
-};
+  this.destroy();
+}
 
 function appearRight() {
-    $("#" + this.element.id)
-      .animate(
-        {
-          opacity: 1,
-          right: "0%"
-        },
-        { duration: 2000, queue: false }
-      );
+  $("#" + this.element.id).animate(
+    {
+      opacity: 1,
+      right: "0%"
+    },
+    { duration: 2000, queue: false }
+  );
 
-    this.destroy();
-};
+  this.destroy();
+}
 
-const appearRightWaypoints = $(".effect__appear--right").map(
-  function() {
-    new Waypoint({
-      element: document.getElementById(this.id),
-      handler: appearRight,
-      offset: "75%"
-    }); }
-
-);
+const appearRightWaypoints = $(".effect__appear--right").map(function() {
+  new Waypoint({
+    element: document.getElementById(this.id),
+    handler: appearRight,
+    offset: "90%"
+  });
+});
 
 const appearLeftWaypoints = $(".effect__appear--left").map(function() {
   new Waypoint({
     element: document.getElementById(this.id),
     handler: appearLeft,
-    offset: "75%"
+    offset: "90%"
   });
 });
