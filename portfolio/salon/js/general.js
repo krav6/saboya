60529cf3b186121dac1cd64d1185e334d16e9bf3
krav6 <patrik.szabo.92@gmail.com>
2018-04-15 11:12:45 +0900

    [feat] Changed font style and added smooth scrolling

--- portfolio/salon/js/general.js
+++ portfolio/salon/js/general.js
@@ -1,3 +1,14 @@
+$(document).on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
+
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
+});
+
 function appearCenter() {
   $("#" + this.element.id).css({ opacity: "1" });
 
