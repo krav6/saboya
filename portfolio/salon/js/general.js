cef267436b0c2e2c77460a4d26f0d3e3a22508f9
krav6 <patrik.szabo.92@gmail.com>
2018-04-15 14:00:43 +0900

    [fix] Fixed carousel not to trigger smooth scrolling

--- portfolio/salon/js/general.js
+++ portfolio/salon/js/general.js
@@ -1,6 +1,10 @@
 $(document).on("click", 'a[href^="#"]', function(event) {
   event.preventDefault();
 
+  if($.attr(this, "href").indexOf("carousel") !== -1){
+    return;
+  }
+
   $("html, body").animate(
     {
       scrollTop: $($.attr(this, "href")).offset().top
